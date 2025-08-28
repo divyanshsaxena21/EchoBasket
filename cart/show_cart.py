f087ceced5355e07c93dc38824974bca528bf0db
unknown <divyansh_saxena@yahoo.com>
2025-08-28 12:09:36 +0530

    new

--- cart/show_cart.py
+++
@@ -1,7 +0,0 @@
-def show_cart(self):
-    if not self.items:
-        print("Cart is empty.")
-    else:
-        print("Items in cart:")
-        for idx, item in enumerate(self.items, 1):
-            print(f"{idx}. {item}")
